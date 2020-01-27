71055eed1c59fa99ce87ed48bd60f8a0a5c39222
Jan Lukas Gernert <jangernert@gmail.com>
2020-01-27 17:32:17 +0100

    fix corrupt filename

--- src/article.rs
+++ src/article.rs
@@ -19,7 +19,7 @@ impl Article {
         if let Some(ref html) = self.html {
             if let Ok(()) = std::fs::create_dir_all(&path) {
                 let mut file_name = match self.title.clone() {
-                    Some(file_name) => file_name,
+                    Some(file_name) => file_name.replace("/", "_"),
                     None => "Unknown Title".to_owned(),
                 };
                 file_name.push_str(".html");
